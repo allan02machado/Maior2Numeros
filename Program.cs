88b6144c4e0b6d630944d54a7df26a31624f0ba1
allan02machado <70041162+allan02machado@users.noreply.github.com>
2020-11-04 15:28:31 -0300

    Este commit exibe a resposta do exercício

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Maior2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int valor1;
+        int valor2;
+
+        Console.WriteLine("Olá, seja bem-vindo ao compara dois números.");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("Digite o primeiro número: ");
+        valor1 = Convert.ToInt32(Console.ReadLine());
+
+        valor2 = valor1;
+
+        Console.Write("Digite o segundo número: ");
+        valor2 = Convert.ToInt32(Console.ReadLine());
+        Console.ResetColor();
+        Console.WriteLine();
+        Console.WriteLine("Obrigado...");
+
+        if(valor1 > valor2)
+        {
+            valor2 = valor1;
+        }
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("O maior número é: "+valor2);
+        Console.ResetColor();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Maior2Numeros
 Resposta ao exercício "Maior2Numeros"
+
+
+**Enunciado**
+
+Receba dois números. Exiba o maior.
+
+Ex.:
+
+Digite o primeiro número..: 7
+Digite o segundo número...: 15
+O maior número é 15
+
+**Como executar:**
+
+Abra o programa e digite:
+
+dotnet run
